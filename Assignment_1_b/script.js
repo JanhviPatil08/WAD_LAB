554cae677b66a2131fb14d9b48d4cf7d15922816
JanhviPatil08 <janhvinpatil@gmail.com>
2025-03-01 14:40:10 +0530

    Initial commit - added project files

--- Assignment_1_b/script.js
+++ Assignment_1_b/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 $(document).ready(function(){
     const form  = $("#registrationForm");
     $("#sub").click( function(){
@@ -18,3 +19,25 @@ $(document).ready(function(){
         form.reset();
     });
 });
+=======
+$(document).ready(function(){
+    const form  = $("#registrationForm");
+    $("#sub").click( function(){
+
+        //get user input
+        const user = {
+            name: $("#name").val(),
+            email: $("#email").val(),
+        };
+
+
+        //Store in local storage
+        let users = JSON.parse(localStorage.getItem("users")) || [];
+        
+        users.push(user);
+        localStorage.setItem("users", JSON.stringify(users));
+
+        form.reset();
+    });
+});
+>>>>>>> 15fe48f (Committed pending changes)
